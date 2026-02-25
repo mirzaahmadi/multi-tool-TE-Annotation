6fe3bb41fc0bf6a70ef8026d4c979ab9265a6a38
Mirza Ahmadi <mamahmadi727@gmail.com>
2026-02-25 16:30:12 -0500

    Fix script execution comment in setup_databases.sh

---
+++ setup_databases.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# To run: ./setup_databases.sh
+
+set -euo pipefail
+
+# Create main directory
+mkdir -p Databases
+cd Databases
+
+# -----------------------------
+# Download Pfam database files
+# -----------------------------
+wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.dat.gz
+wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
+
+# -----------------------------
+# Create pfamdb and unpack
+# -----------------------------
+mkdir -p pfamdb
+
+gunzip -c Pfam-A.hmm.dat.gz > pfamdb/Pfam-A.hmm.dat
+gunzip -c Pfam-A.hmm.gz > pfamdb/Pfam-A.hmm
+
+# Remove compressed files
+rm Pfam-A.hmm.gz Pfam-A.hmm.dat.gz
+
+# -----------------------------
+# Clone pfam_scan
+# -----------------------------
+git clone https://github.com/aziele/pfam_scan
+
+echo "Pfam database and pfam_scan successfully installed in Databases/"
